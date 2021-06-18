ec5dfa7e9f5c4d713a111c920213f17adea8d481
VaidaBurk <v.burkauskaite@gmail.com>
2021-06-18 23:33:20 +0300

    Initial commit

---
+++ WarehouseApp/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace WarehouseApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var warehouse = new Warehouse();
+            string command = "";
+            Console.WriteLine("Enter command:");
+            Console.WriteLine("'add' to add new item. Use space to add list of items;");
+            Console.WriteLine("'remove' to remove item. Use space to remove list of items;");
+            Console.WriteLine("'list' to print all items;");
+            Console.WriteLine("'exit' to close application.");
+            Console.WriteLine();
+
+            while (command != "exit")
+            {
+                command = Console.ReadLine();
+                
+                if (command.StartsWith("add"))
+                {
+                    String[] input = command.Split(" ");
+                    for (int i = 1; i < input.Length; i++)
+                    {
+                    string item = input[i];
+                    warehouse.Add(item);
+                    }
+                }
+
+                if (command.StartsWith("remove"))
+                {
+                    String[] input = command.Split(" ");
+                    for (int i = 1; i < input.Length; i++)
+                    {
+                        string item = input[i];
+                        warehouse.RemoveItem(item);
+                    }
+                }
+
+                if (command == "list")
+                {
+                    warehouse.List();
+                }
+
+            }
+        }
+    }
+}
---
+++ WarehouseApp/Warehouse.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WarehouseApp
+{
+    class Warehouse
+    {
+        private List<string> Items = new List<string>();
+
+        public void Add(string item)
+        {
+            Items.Add(item);
+        }
+
+        public void List()
+        {
+            int warehouseSize = Items.Count;
+            if (warehouseSize > 0)
+            {
+                var GroupedItems = Items.GroupBy(item => item)
+                .Select(group => new { item = group.Key, quantity = group.Count() });
+                
+                foreach (var group in GroupedItems)
+                {
+                    Console.WriteLine(group.item + ", quantity: " + group.quantity);
+                }
+
+                //Console.WriteLine(String.Join(", ", GroupedItems));    // .Join converts list to separate strings
+            }
+            else
+            {
+                Console.WriteLine("sorry, the warehouse is empty!");
+            }
+        }
+
+        public void RemoveItem(string item)
+        {
+            if (!Items.Contains(item)) 
+            {
+                Console.WriteLine("sorry, no such item in the warehouse!");
+            }
+            else
+            {
+                int itemNo = Items.IndexOf(item);
+                Items.RemoveAt(itemNo);
+            }
+        }
+    }
+}
